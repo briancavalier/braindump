bc6063479919a67d8dd45a5c22a1361d0dda8761
Brian Cavalier <brian@briancavalier.com>
2024-05-03 09:28:06 -0400

    Removed unneeded _return field

--- packages/fx/src/handler/handler.ts
+++ packages/fx/src/handler/handler.ts
@@ -24,20 +24,19 @@ export class Handler<E, A, S = void> {
     private readonly handlers: ReadonlyMap<PropertyKey, (e: unknown, s: S) => Fx<unknown, Step<unknown, unknown, S>>> = new Map(),
     private readonly _initially?: FxIterable<unknown, S>,
     private readonly _finally?: (s: S) => FxIterable<unknown, void>,
-    private readonly _return?: (r: A, s: S) => unknown
   ) {}
 
   initially<IE, S>(i: FxIterable<IE, S>): Initially<IE | E, A, S> {
-    return new Handler(this.fx, this.forkable, this.handlers as any, i as any, this._finally as any, this._return as any)
+    return new Handler(this.fx, this.forkable, this.handlers as any, i as any, this._finally as any)
   }
 
   on<Eff extends EffectType, OE, R2 = never>(e: Eff, f: (e: InstanceType<Eff>['arg'], s: S) => Fx<OE, Step<InstanceType<Eff>['R'], R2, S>>): On<Exclude<E, InstanceType<Eff>> | OE, A | R2, S> {
     const handlers = new Map(this.handlers).set(e.id as PropertyKey, f)
-    return new Handler(this.fx, this.forkable, handlers, this._initially, this._finally, this._return) as any
+    return new Handler(this.fx, this.forkable, handlers, this._initially, this._finally) as any
   }
 
   finally<FE>(f: (s: S) => FxIterable<FE, void>): Finally<E | FE, A, S> {
-    return new Handler(this.fx, this.forkable, this.handlers, this._initially, f, this._return)
+    return new Handler(this.fx, this.forkable, this.handlers, this._initially, f)
   }
 
   return<R>(r: (r: A, s: S) => R): Fx<E, R>
