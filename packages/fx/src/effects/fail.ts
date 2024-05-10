cfd5270191fb28d99e1eefec1296e1196d2b70ee
Brian Cavalier <brian@briancavalier.com>
2024-05-10 15:18:11 -0400

    Control handlers resume using provided resume function, return by simply returning

--- packages/fx/src/effects/fail.ts
+++ packages/fx/src/effects/fail.ts
@@ -1,6 +1,6 @@
 import { Effect, Fx, fx, ok } from '../fx'
 // eslint-disable-next-line import/no-cycle
-import { control, done, resume } from '../handler'
+import { control } from '../handler'
 
 export class Fail<const E> extends Effect('fx/Fail')<E, never> { }
 
@@ -9,19 +9,19 @@ export const fail = <const E>(e: E) => new Fail(e)
 export const catchIf = <const F>(match: (x: unknown) => x is F) =>
   <const E, const A>(f: Fx<E, A>) =>
     f.pipe(
-      control(Fail, (e) => fx(function* () {
-        return match(e) ? done(e) : resume(yield* fail(e))
+      control(Fail, (resume, e) => fx(function* () {
+        return match(e) ? e : resume(yield* fail(e))
       }))
     ) as Fx<Exclude<E, Fail<F>>, A | UnwrapFail<E>>
 
 export const catchAll = <const E, const A>(f: Fx<E, A>) =>
   f.pipe(
-    control(Fail, e => ok(done(e)))
+    control(Fail, (_, e) => ok(e))
   ) as Fx<Exclude<E, Fail<any>>, A | UnwrapFail<E>>
 
 export const catchFail = <const E, const A>(f: Fx<E, A>) =>
   f.pipe(
-    control(Fail, e => ok(done(new Fail(e))))
+    control(Fail, (_, e) => ok(new Fail(e)))
    ) as Fx<Exclude<E, Fail<any>>, A | Extract<E, Fail<any>>>
 
 type UnwrapFail<F> = F extends Fail<infer E> ? E : never
--- packages/fx/src/handler/index.ts
+++ packages/fx/src/handler/index.ts
@@ -1,2 +1,1 @@
-export * from './Continue'
 export * from './Handler'
